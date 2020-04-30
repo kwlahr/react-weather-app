90c27af124fc3655ae79dde43410a7eb701f32bc
kwlahr <kwlahr@gmail.com>
2020-04-29 18:54:35 -0600

    added styling to titles component

--- client/src/components/Titles.js
+++ client/src/components/Titles.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Card } from "reactstrap";
 
 const Titles = () => (
   <div>
-    <h1>Weather App</h1>
-    <p>Search weather by city</p>
+    <Card
+      color="dark"
+      className="text-white"
+      style={{ textAlign: "center", fontSize: "1.5rem" }}
+    >
+      <h1 className="display-3">Weather App</h1>
+      <p className="lead">Search weather by city</p>
+    </Card>
   </div>
 );
 
